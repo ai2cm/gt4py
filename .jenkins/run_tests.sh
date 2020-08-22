bd8145774e6d745498487414ae1b457e824d5957
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-08-21 20:34:14 -0700

    pin pytest version

--- .jenkins/run_tests.sh
+++ .jenkins/run_tests.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 pip3 install --upgrade wheel
-pip3 install pytest
+pip3 install pytest==5.2
 git clone https://github.com/gridtools/gt4py.git gt4py
 pip3 install --no-cache-dir -e gt4py
 python3 -m gt4py.gt_src_manager install
