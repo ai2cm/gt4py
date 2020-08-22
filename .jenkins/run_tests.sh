286c82b54bf63184613fa49ee122d6fda44c6c02
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-08-21 20:38:49 -0700

    pytest package version conflicts

--- .jenkins/run_tests.sh
+++ .jenkins/run_tests.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 pip3 install --upgrade wheel
+python3 -m pip install --user virtualenv
+python3 -m venv gt4pyenv
+source gt4pyenv/bin/activate
 pip3 install pytest==5.2
 git clone https://github.com/gridtools/gt4py.git gt4py
 pip3 install --no-cache-dir -e gt4py
