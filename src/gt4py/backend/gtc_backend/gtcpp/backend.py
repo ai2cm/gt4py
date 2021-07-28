1fde25457f163ea08361893124998826b90af5bf
Eddie Davis <eddied@vulcan.com>
2021-07-28 23:25:51 +0000

    Skip GreedyMerging instead of graph_merge_horizontal_executions

--- src/gt4py/backend/gtc_backend/gtcpp/backend.py
+++ src/gt4py/backend/gtc_backend/gtcpp/backend.py
@@ -37,10 +37,8 @@ from gtc import gtir_to_oir
 from gtc.common import DataType
 from gtc.gtcpp import gtcpp, gtcpp_codegen, oir_to_gtcpp
 from gtc.passes.gtir_pipeline import GtirPipeline
-from gtc.passes.oir_dace_optimizations.horizontal_execution_merging import (
-    graph_merge_horizontal_executions,
-)
 from gtc.passes.oir_optimizations.caches import FillFlushToLocalKCaches, KCacheDetection
+from gtc.passes.oir_optimizations.horizontal_execution_merging import GreedyMerging
 from gtc.passes.oir_pipeline import OirPipeline
 
 
@@ -58,7 +56,7 @@ class GTCGTExtGenerator:
         gtir = GtirPipeline(DefIRToGTIR.apply(definition_ir)).full()
         oir = OirPipeline(gtir_to_oir.GTIRToOIR().visit(gtir)).full(
             skip=[
-                graph_merge_horizontal_executions,
+                GreedyMerging,
                 KCacheDetection,
                 FillFlushToLocalKCaches,
             ]
