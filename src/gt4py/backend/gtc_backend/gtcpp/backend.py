0696dd565654a816f4811f3724b0b66434314ba5
Eddie Davis <eddied@vulcan.com>
2021-07-29 15:48:41 +0000

    Revert GreedyMerging change

--- src/gt4py/backend/gtc_backend/gtcpp/backend.py
+++ src/gt4py/backend/gtc_backend/gtcpp/backend.py
@@ -37,8 +37,10 @@ from gtc import gtir_to_oir
 from gtc.common import DataType
 from gtc.gtcpp import gtcpp, gtcpp_codegen, oir_to_gtcpp
 from gtc.passes.gtir_pipeline import GtirPipeline
+from gtc.passes.oir_dace_optimizations.horizontal_execution_merging import (
+    graph_merge_horizontal_executions,
+)
 from gtc.passes.oir_optimizations.caches import FillFlushToLocalKCaches, KCacheDetection
-from gtc.passes.oir_optimizations.horizontal_execution_merging import GreedyMerging
 from gtc.passes.oir_pipeline import OirPipeline
 
 
@@ -56,7 +58,7 @@ class GTCGTExtGenerator:
         gtir = GtirPipeline(DefIRToGTIR.apply(definition_ir)).full()
         oir = OirPipeline(gtir_to_oir.GTIRToOIR().visit(gtir)).full(
             skip=[
-                GreedyMerging,
+                graph_merge_horizontal_executions,
                 KCacheDetection,
                 FillFlushToLocalKCaches,
             ]
